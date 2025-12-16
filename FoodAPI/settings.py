4781cbf8d813f4b9814dd89b525ed4f87b84dde2
Dark <izzxolmatov14@gmail.com>
2025-12-16 20:13:24 +0500

    initial

--- FoodAPI/settings.py
+++ FoodAPI/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'drf_yasg',
     'django_filters',
     'FoodApp.apps.FoodappConfig',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -152,5 +152,12 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
